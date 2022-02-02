6dc640e0f1589b91f8d322785b7c0665bf62cbb0
snxhz01 <77512847+snxhz01@users.noreply.github.com>
2022-02-02 15:07:31 -0500

    algorithm
    gives user's desired term, using the algorithm that was hard coded into the program

--- nth_digit.py
+++ nth_digit.py
@@ -2,3 +2,20 @@ import math
 
 print('Fibonacci Sequence: The "nth" Term\n')
 n = input('Enter term in its integer form: ')
+
+if int(n) > 0:
+    while True:
+        try:
+            val = int(n)
+            result = round((1.618034 ** val - (1-1.618034)**6)/math.sqrt(5))
+            print('The '+str(n)+'th term is: ' + str(result))
+            break;
+        except ValueError:
+            try:
+                float(n)
+                print('Error: '+n+' is not valid input\n Please provide input in INTEGER form\n')
+                break;
+            except ValueError:
+                print('Error: '+n+' is not valid input\n Please provide input in INTEGER form\n')
+else:
+    print('Error: There is no ' + n + 'th number in the sequence\n Please provide a valid input\n')
